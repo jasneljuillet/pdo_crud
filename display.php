f31a3f7eb5391f93bfe8973522ef23b73c63fa95
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-04 12:22:42 -0500

    Display the data from the database

--- display.php
+++ display.php
@@ -6,8 +6,9 @@
     $req->execute();
     $result = $req->fetchAll();
 
+    echo "Nombre Etudiant ".count($result);
     for($i = 0; $i < count($result); $i++) {
-        echo $result[$i]['email_etudiant'].' <br/>';
+        echo "<p> Email </p>".$result[$i]['email_etudiant'];
     }
 
 ?>

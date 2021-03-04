f174da5d65bfb2c87f8952b3b6bc2ace2eb18dc2
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-04 12:22:03 -0500

    Delete data from the database

--- delete.php
+++ delete.php
@@ -1,5 +1,12 @@
 <?php 
     require("./serverConnection.php");
 
-
+    try {
+        $req = $db->prepare("DELETE FROM etudiant WHERE id_etudiant = ?");
+        $req->execute([3]);
+        echo " User are successfully delete";
+    } catch(PDOException $error) {
+        echo " cannot delete the user ".$error->getMessage();
+    }
+    
 ?>

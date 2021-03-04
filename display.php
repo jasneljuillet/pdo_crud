5c042a3b5f429e7cab4c461171dd00ac1e00eba2
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-04 11:06:58 -0500

    Display data from the database

--- display.php
+++ display.php
@@ -1,9 +1,13 @@
 <?php 
     require("./serverConnection.php");
 
-    // $req = $db->prepare("SELECT * FROM etudiant");
-    // $req->setFetchMode(PDO::FETCH_ASSOC);
-    // $req->execute();
-    // $result = $req->fetchAll();
+    $req = $db->prepare("SELECT * FROM etudiant");
+    $req->setFetchMode(PDO::FETCH_ASSOC);
+    $req->execute();
+    $result = $req->fetchAll();
+
+    for($i = 0; $i < count($result); $i++) {
+        echo $result[$i];
+    }
 
 ?>

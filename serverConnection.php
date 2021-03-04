feed45b9844d5f8ce68e7966a444f3439a64d69d
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-04 12:00:54 -0500

    connect to db

--- serverConnection.php
+++ serverConnection.php
@@ -5,8 +5,8 @@
     try{
         $db = new PDO("mysql:host=$server;dbname=$uneph", $user, $password);
          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//        echo "You are connected";
+    //    echo "You are connected";
     } catch(PDOException $error) {
-        echo "You are not connected".$error->getMessage();
+        echo "You are not connected ".$error->getMessage();
     }
 ?>
